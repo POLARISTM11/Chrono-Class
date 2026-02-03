68312934464085cb16633dce33afd16ca90d1f32
Polaris <xavierreategui28@gmail.com>
2026-02-03 16:39:28 -0500

    TRABAJO FINAL :V

--- Lenguaje - Proyecto/Chrono Class.cpp
+++ Lenguaje - Proyecto/Chrono Class.cpp
@@ -1,7 +1,6 @@
-// Lenguaje - Proyecto.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 #include <iostream>
 #include <string>
+#include <stdio.h>
 
 #define FILAS 7
 #define COLUMNAS 6
@@ -41,8 +40,8 @@ void registrarCurso(string** matriz) {
     int fila, columna;
     string nombre;
     cout << "\n--- Registrar Curso ---" << endl;
-    cout << "Ingrese bloque horario (0-6): "; cin >> fila;
-    cout << "Ingrese dia (0-5): "; cin >> columna;
+    cout << "Ingrese bloque horario (0.8:00/1.9:00/.../6.2:00): "; cin >> fila;
+    cout << "Ingrese dia (0.Lunes/1.Martes/.../5.Sabado): "; cin >> columna;
 
     if (fila >= 0 && fila < FILAS && columna >= 0 && columna < COLUMNAS) {
         cout << "Nombre del curso: ";
@@ -51,7 +50,7 @@ void registrarCurso(string** matriz) {
 
         string* celda = buscarEspacio(matriz, fila, columna);
         *celda = nombre;
-        cout << "¡Curso registrado con exito!" << endl;
+        cout << "Curso registrado con exito!" << endl;
     }
     else {
         cout << "Error: Indices fuera de rango." << endl;
@@ -76,12 +75,31 @@ void MostrarHorarioCompleto(string** Horario, int HorasTotales, int DiasTotales)
     cout << "\t==========================================================\n";
 }
 
+void MostrarDia(string** Horario, int Horas, int Dias) {
+    int Dia;
+    string DiasSemana[6] = { "Lunes","Martes","Miercoles","Jueves","Viernes","Sabado" };
+
+    do {
+        cout << "\n\tIngrese dia (1=Lunes ... " << Dias << "=Sabado): ";
+        cin >> Dia;
+
+        if (Dia < 1 || Dia > Dias) {
+            cout << "\tDia invalido, porfabor vuelva a ingresar el numero dentro del rango.\n";
+        }
+    } while (Dia < 1 || Dia > Dias);
+
+    cout << "\n\tClases de " << DiasSemana[Dia - 1] << ":\n";
+    for (int hora = 0; hora < Horas; hora++) {
+        cout << "\t" << (hora + 8) << ":00 - " << Horario[hora][Dia - 1] << "\n";
+    }
+}
+
 // --- PARTE 4: PROCESAMIENTO (Haylie) ---
 void contar_horas(string** matriz, int filas, int columnas) {
     string buscar;
     int contador = 0;
 
-    cout << "\n¿De que curso quieres contar las horas?: ";
+    cout << "\nDe que curso quieres contar las horas?: ";
     cin.ignore();
     getline(cin, buscar);
 
@@ -92,37 +110,50 @@ void contar_horas(string** matriz, int filas, int columnas) {
             }
         }
     }
-    cout << "Resultado: " << buscar << " tiene " << contador << " horas." << endl;
+    cout << "Resultado: " << buscar << " tiene " << contador << " hora(s)." << endl;
 }
 
-// --- MAIN ---
 int main() {
     string** horarioPrincipal = crearMatriz();
     inicializar(horarioPrincipal);
 
     int opcion;
     do {
-        cout << "\n>>> SISTEMA CHRONO CLASS <<<\n";
-        cout << "1. Registrar Curso\n2. Ver Horario Completo\n3. Contar Horas\n4. Salir\nOpcion: ";
+        cout << "\n\t=======================================\n";
+        cout << "      \t\tSISTEMA CHRONO CLASS     \n";
+        cout << "\t========================================\n";
+        cout << "\t |1. Registrar curso                  |\n ";
+        cout << "\t |2. Mostrar horario completo         |\n";
+        cout << "\t |3. Mostrar clases de un dia         |\n";
+        cout << "\t |4. Contar horas de un curso         |\n";
+        cout << "\t |5. Salir                            |\n";
+        cout << "\t---------------------------------------\n";
+        cout << "\tElija una opcion (1-5): ";
         cin >> opcion;
 
         switch (opcion) {
-        case 1: registrarCurso(horarioPrincipal); break;
-        case 2: MostrarHorarioCompleto(horarioPrincipal, FILAS, COLUMNAS); break;
-        case 3: contar_horas(horarioPrincipal, FILAS, COLUMNAS); break;
-        case 4: cout << "Saliendo..."; break;
+        case 1:
+            registrarCurso(horarioPrincipal);
+            break;
+        case 2:
+            MostrarHorarioCompleto(horarioPrincipal, FILAS, COLUMNAS);
+            break;
+        case 3:
+            MostrarDia(horarioPrincipal, FILAS, COLUMNAS);
+            break;
+        case 4:
+            contar_horas(horarioPrincipal, FILAS, COLUMNAS);
+            break;
+        case 5:
+            cout << "Saliendo...";
+            break;
+        default:
+            cout << "\n\tOpcion no valida. porfabor volver a ingresar" << endl;
+            break;
         }
-    } while (opcion != 4);
+
+    } while (opcion != 5);
 
     liberarMemoria(horarioPrincipal);
     return 0;
 }
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
